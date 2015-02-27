c59f50cfef7648690a08c5c774324d1691f6f2fb
Nathan Samano <nsv9982@saintjoe.edu>
2015-02-27 13:40:20 -0500

    Added getMin, getMax, find

--- BSTtree.js
+++ BSTtree.js
@@ -116,3 +116,35 @@ function getSmallest(node) {
       return getSmallest(node.left);
    }
 }
+
+function getMin() {
+  var current = this.root;
+  while (!(current.left == null)) {
+    current = current.left;	
+  }
+  return current.data;
+}
+
+function getMax() {
+  var current = this.root;
+  while (!(current.right == null)) {
+    current = current.right;
+  }
+  return current.data;
+}
+
+function find(data) {
+  var current = this.root;
+  while (current.data != data) {
+    if (data < current.data) {
+      current = current.left;
+    }
+    else {
+      current = current.right;
+    }
+    if (current == null) {
+      return null;
+    }
+  }
+  return current;
+}
